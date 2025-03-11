660c0b7cf1f69c7b1c393ea2df897164f1aaa4ee
Adrien <adrienleyval4@gmail.com>
2025-03-11 16:24:27 +0100

    feat: add mongodb connection with mongoose

--- api/src/config.ts
+++ api/src/config.ts
@@ -3,12 +3,30 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 interface Config {
+    dbUser: string;
+    dbPassword: string;
+    dbName: string;
+    dbHost: string;
+    dbCluster: string;
     uri: string;
     port: number;
 }
 
+function getEnvVar(key: string, defaultValue?: string): string {
+  const value = process.env[key] || defaultValue;
+  if (!value) {
+    throw new Error(`Missing environement variable ${key}.`);
+  }
+  return value;
+}
+
 const config: Config = {
-  uri: process.env.DATABASE_URI || 'mongodb://localhost:5000',
+  dbUser : getEnvVar('DB_USER'),
+  dbPassword: getEnvVar('DB_PASSWORD'),
+  dbName: getEnvVar('DB_NAME'),
+  dbHost: getEnvVar('DB_HOST'),
+  dbCluster: getEnvVar('DB_CLUSTER'),
+  uri: `mongodb+srv://${getEnvVar('DB_USER')}:${getEnvVar('DB_PASSWORD')}@${getEnvVar('DB_HOST')}/${getEnvVar('DB_NAME')}?retryWrites=true&w=majority&appName=${getEnvVar('DB_CLUSTER')}` || 'mongodb://localhost:5000',
   port: process.env.PORT ? parseInt(process.env.PORT) : 3000
 };
 
