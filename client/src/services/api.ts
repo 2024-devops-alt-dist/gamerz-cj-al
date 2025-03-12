b4961215719bf1bf39e752fafc696cb508687b31
Camille Janin <cmille.jn@gmail.com>
2025-03-12 12:02:51 +0100

    feat: add login/register form

---
+++ client/src/services/api.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+import { IUser } from "../models/IUser";
+
+const api = axios.create({
+    url: 'http://localhost:3000/gamerz'
+})
+
+export const getUsers = () => api.get<IUser[]>('/users');
+export const getUsersId = (id: string) => api.get<IUser>(`/users/${id}`);
+export const addUser = () => api.post<IUser>('/users');
