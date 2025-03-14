c50b4367be87a214806e3fdfcfb0b959aab7d36f
Adrien <adrienleyval4@gmail.com>
2025-03-14 12:13:34 +0100

    feat: add login and register routes

--- api/src/index.ts
+++ api/src/index.ts
@@ -5,6 +5,7 @@ import dbConnect from './db/db';
 import swaggerUi from 'swagger-ui-express';
 import swaggerDocs from './swaggerConfig';
 import userRoutes from './routes/userRoutes';
+import authRoutes from './routes/authRoutes';
 
 const app: Application = express();
 const port = config.port;
@@ -14,6 +15,7 @@ app.use(express.json());
 dbConnect();
 
 app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use('/api', authRoutes);
 app.use('/api', userRoutes);
 
 app.listen(port, () => {
