005d8eb54f664566e9d0e6647473317cec8bd767
Camille Janin <cmille.jn@gmail.com>
2025-04-07 11:10:31 +0200

    feat: login form / register form / create home page

---
+++ client/src/pages/rooms/room3.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Room3: React.FC = () => {
+    
+    return (
+        <>
+            <h1>Room 3</h1>
+        </>
+        
+    );
+};
+
+export default Room3;
